8a06c068bb312f12dd7f8b85b630f549bf6dc4fe
Kim Young Jae <corea_kyj@naver.com>
2018-01-29 10:30:23 +0900

    Version 1.1

--- file_basic_properties.py
+++ file_basic_properties.py
@@ -1,5 +1,20 @@
 import os, subprocess, hashlib, simplejson
 
+import multiprocessing as mp
+
+from settings import *
+
+def create_file_list ( root ) :
+    ret_list = []
+    for path, dirs, files in os.walk(root) :
+        for file in files :
+            ext = os.path.splitext(file)[-1]
+            if ext == '.vir' :
+                full_path = os.path.join(path, file)
+                ret_list.append(full_path)
+
+    return ret_list
+
 def get_hash_str( file_path, block_size = 8192 ) :
     md5, sha_1 = hashlib.md5(), hashlib.sha1()
     with open(file_path, 'rb') as f :
@@ -25,7 +40,7 @@ def get_ssdeep( file_path ) :
     ssdeep_result = subprocess.check_output('ssdeep {file_path}'.format(file_path=file_path)).decode('utf-8').strip().split('\n')[-1].split(',')[0]
     return ssdeep_result
 
-def basic_properties( file_path ) :
+def make_json( file_path ) :
     ret_dic = {}
     # md5, sha1
     ret_dic['MD5'], ret_dic['SHA-1'] = get_hash_str( file_path )
@@ -35,13 +50,18 @@ def basic_properties( file_path ) :
     ret_dic['File Size'] = get_file_size(file_path)
     # SSDeep
     ret_dic['SSDeep'] = get_ssdeep(file_path)
-    return ret_dic
+    json_path, json_file_name = os.path.split(file_path)
+    json_path.replace('malware','json')
+    json_file_name.replace('.vir','.json')
+    if not os.exists(json_path) :
+        os.makedirs(json_path)
+    json_full_path = os.path.join(json_path, json_file_name)
+    with open(json_full_path, 'w') as f:
+        simplejson.dump(ret_dic, f)
 
-if __name__ == '__main__' :
-    #test_file_path = 'D:\\byeongal\\file_basic_properties\\DHelper.exe'
-    #test_file_path = 'D:\\byeongal\\file_basic_properties\\main.cpp'
-    test_file_path = 'D:\\byeongal\\file_basic_properties\\boj_10951.exe'
-
-    json_result=simplejson.dumps(basic_properties(test_file_path))
-    with open('resut.json', 'w', encoding="utf8") as f:
-        simplejson.dump(basic_properties(test_file_path), f, ensure_ascii=False)
+def run() :
+    mp.freeze_support()
+    file_list = create_file_list(MALWARE_PATH)
+    print("Total Malware Count : {}".format(len(file_list)))
+    p = mp.Pool(CPU_COUNT)
+    p.map(make_json, file_list)
